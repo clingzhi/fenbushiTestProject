4eed2948cbc47949dfea5cd61b576e7459025872
clingzhi <shanyaojt@163.com>
2020-03-02 19:38:52 +0800

    Initial commit:gradProject

---
+++ grad-trade-consumer/src/test/java/cc/ysf/dx/AppTest.java
@@ -0,0 +1,20 @@
+package cc.ysf.dx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
